d4ef2182fbbe0926d6a5dfb5897a314f20870661
Talha Hasan Zia <talhahasanzia@hotmail.com>
2018-05-08 17:16:51 +0500

    clock working statically

--- ViewsPlayground/app/src/main/java/com/playground/viewsplayground/analog_clock/AnalogPlayableClock.java
+++ ViewsPlayground/app/src/main/java/com/playground/viewsplayground/analog_clock/AnalogPlayableClock.java
@@ -1,20 +1,18 @@
 package com.playground.viewsplayground.analog_clock;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.text.format.DateUtils;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.TimeUtils;
 import android.view.View;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import com.playground.viewsplayground.R;
 
 /**
  * Created by Talha Hasan Zia on 08-May-18.
@@ -23,6 +21,16 @@ import java.util.Calendar;
  * <b>Public Methods:</b><p></p> Only listing to public methods usage.
  */
 public class AnalogPlayableClock extends View {
+
+    private static final String TAG = "ANACLOCK";
+    private int hour = 4, minutes = 30;
+    private double smallHandStartX, smallHandEndX, bigHandStartX, bigHandEndX, smallHandStartY, smallHandEndY, bigHandStartY, bigHandEndY;
+    private float smallHandLength, bigHandLength, dialRadius;
+    private float smallHandWidth, bigHandWidth, dialWidth;
+    private float validPadding;
+    private double smallHandSlope, bigHandSlope;
+    private boolean isDrawingAllowed = true;
+
     public AnalogPlayableClock(Context context) {
         super(context);
     }
@@ -40,67 +48,214 @@ public class AnalogPlayableClock extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    private void init(Context context, AttributeSet attributeSet)
+    {
+        if (attributeSet != null) {
 
-    private void init() {
+            TypedArray attributeArray = context.obtainStyledAttributes(attributeSet, R.styleable.AnalogPlayableClock);
 
+            attributeArray.recycle();
+            
+        }
 
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        isDrawingAllowed = true;
+
+        validPadding = getValidPadding();
+
+
+        // ensure that width and height is same
+        int measuredHeight = 0;
+        int measuredWidth = 0;
+        try {
+            measuredWidth = measureWidth(widthMeasureSpec);
+            measuredHeight = measureHeight(heightMeasureSpec);
+
+            if (measuredHeight != measuredWidth) {
+                isDrawingAllowed = false;
+                throw new Exception(AnalogPlayableClock.class.getCanonicalName() + ": Height and Width of this view must be same.");
+            }
+
+            if (normalizeValue(validPadding) > measuredWidth / 2) {
+                isDrawingAllowed = false;
+                throw new Exception(AnalogPlayableClock.class.getCanonicalName() + ": Invalid padding values");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    private int measureHeight(int measureSpec) throws Exception {
+        // ensure that width and height is same
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        int result;
+
+        if (specMode == MeasureSpec.EXACTLY) { // only allowed fixed size or match parent
+            result = specSize;
+        } else {
+            isDrawingAllowed = false;
+            throw new Exception(AnalogPlayableClock.class.getCanonicalName() + ": Height of this view cannot be wrap_content");
+        }
+
+        return result;
+    }
+
+    private int measureWidth(int measureSpec) throws Exception {
+
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        int result;
+
+        if (specMode == MeasureSpec.EXACTLY) { // only allowed fixed size or match parent
+            result = specSize;
+        } else {
+            isDrawingAllowed = false;
+            throw new Exception(AnalogPlayableClock.class.getCanonicalName() + ": Height of this view cannot be wrap_content");
+        }
+
+        return result;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        float viewMidX = getWidth() / 2;
+        float viewMidY = getHeight() / 2;
+
+        if (isDrawingAllowed) {
+
+            dialRadius = getWidth() / 2.25f;
+            smallHandStartX = viewMidX;
+            smallHandStartY = viewMidY;
+
+            bigHandStartX = smallHandStartX;
+            bigHandStartY = smallHandStartY;
+
+
+            bigHandLength = getHeight() - (getHeight() / 1.45f);
+            smallHandLength = bigHandLength * 0.6f;
+
+
+            smallHandWidth = getWidth() * 0.02f;
+            bigHandWidth = getWidth() * 0.01f;
+            dialWidth = smallHandWidth;
+
+            drawDial(canvas, viewMidX, viewMidY);
+
+            drawBigHand(canvas, viewMidX, viewMidY);
+
+            drawSmallHand(canvas, viewMidX, viewMidY);
+
+
+        } else {
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.RED);
+            paint.setTextSize(getHeight() * 0.1f);
+            canvas.drawText("Error, See logs.", 4, viewMidY, paint);
+        }
+    }
+
+    private void drawBigHand(Canvas canvas, float viewMidX, float viewMidY) {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(bigHandWidth);
+        paint.setColor(Color.BLUE);
+
+
+        bigHandSlope = getMinutesAngleFromTime();
+        bigHandEndX = (bigHandLength * Math.cos(bigHandSlope)) + getWidth() / 2;
+        bigHandEndY = (bigHandLength * Math.sin(bigHandSlope)) + getHeight() / 2;
+
+        canvas.drawLine(viewMidX, viewMidY, (float) bigHandEndX, (float) bigHandEndY, paint);
+    }
+
+    private void drawSmallHand(Canvas canvas, float viewMidX, float viewMidY) {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(smallHandWidth);
+        paint.setColor(Color.RED);
+
+        smallHandSlope = getHoursAngleFromTime();
+        smallHandEndX = (smallHandLength * Math.cos(smallHandSlope)) + viewMidX;
+        smallHandEndY = (smallHandLength * Math.sin(smallHandSlope)) + viewMidY;
+
+        canvas.drawLine(viewMidX, viewMidY, (float) smallHandEndX, (float) smallHandEndY, paint);
+
+    }
+
+    private void drawDial(Canvas canvas, float viewMidX, float viewMidY) {
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(12);
-        canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, 300, paint);
-        paint.setStrokeWidth(24);
-        paint.setColor(Color.RED);
-        canvas.drawLine(getWidth() / 2, getHeight() / 2, getWidth() / 2, getHeight() - (getHeight() / 1.55f), paint);
+        paint.setStrokeWidth(dialWidth);
+        canvas.drawCircle(viewMidX, viewMidY, dialRadius, paint);
+    }
 
-        paint.setStrokeWidth(20);
-        paint.setColor(Color.BLUE);
+    private double getMinutesAngleFromTime() {
 
-        double length = 280;
+        Log.d(TAG, "getMinutesAngleFromTime: hour: " + hour);
+        Log.d(TAG, "getMinutesAngleFromTime: mins: " + minutes);
 
-        Calendar calendar=Calendar.getInstance();
+        if (hour > 12)
+            hour = hour - 12;
 
-        calendar.set(Calendar.HOUR, 12);
-        calendar.set(Calendar.MINUTE, 30);
+        double totalInput = (hour * 60) + minutes;
 
-        double angleFromTime=getAngleFromTime(calendar.getTimeInMillis());
-        Log.d("ANACLOCK", "onDraw: "+angleFromTime);
-        double endX =  (length * Math.cos(angleFromTime))+getWidth() / 2;
-        double endY =  (length * Math.sin(angleFromTime))+getHeight() / 2;
 
-        Log.d("ANACLOCK", "onDraw: "+ endX+","+endY);
-        canvas.drawLine(getWidth() / 2, getHeight() / 2, (float)endX, (float) endY, paint);
+        double degreeInAngles = totalInput * 6;
+
+        return Math.toRadians(degreeInAngles - 90);
 
 
     }
 
-    private double getAngleFromTime(long timeInMilliseconds) {
-        Calendar calendar=Calendar.getInstance();
-        calendar.setTimeInMillis(timeInMilliseconds);
+    private double getHoursAngleFromTime() {
 
-        int hour=calendar.get(Calendar.HOUR);
-        int minutes=calendar.get(Calendar.MINUTE);
+        Log.d("ANACLOCK", "getMinutesAngleFromTime: hour: " + hour);
 
-        Log.d("ANACLOCK", "getAngleFromTime: hour: "+hour);
-        Log.d("ANACLOCK", "getAngleFromTime: mins: "+minutes);
-
-        if(hour>12)
-            hour=hour-12;
-
-        double totalInput=(hour*60)+minutes;
-        
-
-        double degreeInAngles=totalInput*6;
-
-        Log.d("ANACLOCK", "getAngleFromTime: "+degreeInAngles);
-
-        return  Math.toRadians(degreeInAngles-90);
+        if (hour > 12)
+            hour = hour - 12;
 
 
+        double degreeInAngles = hour * 30;
+
+
+        return Math.toRadians(degreeInAngles - 90);
+
+
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+        invalidate();
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+        invalidate();
+    }
+
+    private float getValidPadding() {
+        float[] paddings = {getPaddingLeft(), getPaddingTop(), getPaddingRight(), getPaddingBottom(), getPaddingStart(), getPaddingEnd()};
+
+        float validPadding = paddings[0];
+
+        for (int i = 1; i < paddings.length; i++) {
+            if (validPadding < paddings[i])
+                validPadding = paddings[i];
+        }
+
+        return validPadding;
+    }
+
+    private float normalizeValue(float val) {
+        return val < 0 ? val * (-1) : val;
     }
 }
